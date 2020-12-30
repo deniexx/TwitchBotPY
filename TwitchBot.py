baf0b81c3d497fc97cb46d8566984ea8ec2cc4ad
Denis Dimitrov <61838567+deniexx@users.noreply.github.com>
2020-12-30 19:41:33 +0000

    Update TwitchBot.py

--- TwitchBot.py
+++ TwitchBot.py
@@ -2,17 +2,20 @@ import re
 from twitchio.ext import commands
 import asyncio
 import random
+import json
 
-CHANNELS = ["#YourChannel"]  # channels that the bot will connect [must be list or tuple]
-NICK = "NickOfBot"  # your bot channel username
+CHANNELS = ["#deniexxx"]  # channels that the bot will connect [must be list or tuple]
+NICK = "metchebot"  # your bot channel username
 
 bot = commands.Bot(
-    irc_token="oauth:YourAuthToken",  # your oauth code
-    client_id="YourClientId",  # your app id
+    irc_token="oauth:o7tivn21xsuawgioyxajdcm8puve7u",  # your oauth code
+    client_id="a80ian18gjdpz41lgt1j2wctv8pf2u",  # your app id
     nick=NICK,
     prefix="!",  # command prefix
     initial_channels=CHANNELS
 )
+
+trustedUsersFile = "trustedUsers.txt"
 
 trustedUsers = []
 
@@ -22,10 +25,23 @@ intervalMessages = ["You can grab your own MetcheBot source code from: https://g
 # To add more messages to send add a comma and a on a a new line type your text in quotations
 
 
-async def intervalMessage(ws):
+async def intervalmessage(ws):
     for channel in CHANNELS:
         for message in intervalMessages:
             await ws.send_privmsg(channel, message)
+
+    await asyncio.sleep(300)
+    await intervalmessage(ws)
+
+
+def takeUsers():
+    try:
+        f = open(trustedUsersFile, "r")
+        return json.load(f)
+    except Exception as e:
+        print(e)
+        return []
+    # Take all users from file and import them into list
 
 
 @bot.event
@@ -34,9 +50,10 @@ async def event_ready():
     ws = bot._ws
     for channel in CHANNELS:
         await ws.send_privmsg(channel, "/me is coming to save the day")
-        # message to send when he comes online
+        # Message to send when the bot comes online
 
-    await intervalMessage(ws)
+    trustedUsers = takeUsers()
+    await intervalmessage(ws)
 
 
 @bot.event
@@ -46,7 +63,7 @@ async def event_message(ctx):
 
     await bot.handle_commands(ctx)
 
-    # check if there is a link in the message
+    # Check if there is a link in the message
     match = re.search(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]"
                       r"+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>"
                       r"]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<"
@@ -54,18 +71,18 @@ async def event_message(ctx):
                       r"\[\]{};:'\".,<>?«»“”‘’]))", ctx.content)
 
     if match and ctx.author.name\
-            not in trustedUsers and not ctx.author.is_mod:
+            not in f"@{trustedUsers}" and not ctx.author.is_mod:
 
         await ctx.channel.timeout(ctx.author.name, 60, "link post")
         await ctx.channel.send(f"Don't post links @{ctx.author.name}")
-        # times out people who post links
+        # Times out people who post links
 
 
 @bot.command(name="clear")
 async def clear(ctx):
     if ctx.author.is_mod:
         await ctx.channel.clear()
-        # clears the entire chat if the command is used by a moderator
+        # Clears the entire chat if the command is used by a moderator
 
 
 @bot.command(name="ban")
@@ -73,34 +90,44 @@ async def ban(ctx, user=None, *args):
     if ctx.author.is_mod:
         if user and args:
             await ctx.channel.ban(user, " ".join(args))
-            await ctx.send(f"@{user} has been banned for {' '.join(args)}")
+            await ctx.send(f"{user} has been banned for {' '.join(args)}")
 
         else:
             await ctx.send(f'@{ctx.author.name} -> !ban "user" "reason"')
-            # bans a user if command is used by mod
+            # Bans a user if command is used by mod
 
 
 @bot.command(name="unban")
 async def unban(ctx, user):
     if ctx.author.is_mod:
         await ctx.channel.unban(user)
-        # unbans user if command is used by mod
+        # Unbans user if command is used by mod
 
 
 @bot.command(name="trust")
 async def trust(ctx, user):
     if ctx.author.is_mod:
         trustedUsers.append(user)
-        await ctx.send(f"@{user} is permitted to post links")
-        # you can permit a user to post links if command is used by mod
+        await ctx.send(f"{user} is permitted to post links")
+        users = takeUsers()
+        users.append(user)
+        with open(trustedUsersFile, "w+") as f:
+            json.dump(users, f)
+        # You can permit a user to post links if command is used by mod
 
 
 @bot.command(name="untrust")
 async def untrust(ctx, user):
     if ctx.author.is_mod:
         trustedUsers.remove(user)
-        await ctx.send(f"@{user} is no longer permitted to post links")
-        # you can remove the permission from the user if command is used by mod
+
+        users = takeUsers()
+        users.remove(user)
+        f = open(trustedUsersFile, "w+")
+        json.dump(users, f)
+        f.close()
+        await ctx.send(f"{user} is no longer permitted to post links")
+        # You can remove the permission from the user if command is used by mod
 
 
 @bot.command(name="winner")
@@ -108,6 +135,6 @@ async def getwinner(ctx):
     if ctx.author.is_mod:
         users = await bot.get_chatters(str(ctx.channel))
         await ctx.send(f"@{random.choice(users[1])} is the winner")
-        # gets a list of all members and chooses 1 on random
+        # Gets a list of all members and chooses 1 on random
 
 bot.run()
